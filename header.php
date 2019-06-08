5f016dcecd640500d2caa13d2d96e1b515e7d7df
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-08 15:29:19 -0400

    Add header & footer

---
+++ header.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <?php get_template_part('template-parts/headers/the-head'); ?>
+  <body>
+    <?php get_template_part('template-parts/headers/the-header');
