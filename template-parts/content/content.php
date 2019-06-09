fbb161a98c235f86dac0f84b3f3f2cc4c659fabc
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-09 02:37:49 -0400

    Move main h1 into .container in content.php (template-parts)

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -10,8 +10,8 @@
 ?>
 
 <div class="container-fluid">
-  <h1><?php the_title(); ?></h1>
   <div class="container limit-width">
+    <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
   </div>
 </div>
