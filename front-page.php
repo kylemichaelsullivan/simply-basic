2ee43492adc43461cf5670c9ea2b037423cfe3b9
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-08 22:25:32 -0400

    Use get_template_part_by_slug() & update meta data

--- front-page.php
+++ front-page.php
@@ -1,8 +1,16 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for the front page
+ *
+ * @package simplyBasic
+ * @since simplyBasic 0.0.0
+ */
+
+get_header(); ?>
 
 <?php while( have_posts() ) {
   the_post();
-  get_template_part('template-parts/content/content');
+  get_template_part_by_slug();
 } ?>
 
 <?php get_footer();
