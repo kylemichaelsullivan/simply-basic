feb48e86665f6ebeed5f80a35b8310ecbb9deeb8
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-08 22:34:38 -0400

    Add archive.php

--- archive.php
+++ archive.php
@@ -1,8 +1,44 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package simplyBasic
+ * @since 0.0.0
+ */
 
-<?php while( have_posts() ) {
-  the_post();
-  get_template_part('template-parts/content/content');
-} ?>
+get_header();
+?>
 
-<?php get_footer();
+<section id="primary" class="content-area">
+	<main id="main" class="site-main">
+	<?php if( have_posts() ) { ?>
+		<header class="page-header">
+			<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		</header>
+
+		<?php
+		// Start the Loop.
+		while( have_posts() ) {
+			the_post();
+
+			/* Include the Post-Format-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
+			get_template_part('template-parts/content/content', 'excerpt');
+		}
+
+		// Previous/next page navigation.
+		simplyBasic_the_posts_navigation();
+
+	} else {
+		get_template_part('template-parts/content/content', 'none');
+
+	} ?>
+	</main>
+</section>
+
+<?php
+get_footer();
