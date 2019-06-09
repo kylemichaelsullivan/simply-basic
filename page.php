2fdfe480ac2d21421bad944baa82af896a8140ff
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-08 22:24:24 -0400

    Use get_template_part_by_slug() & update meta data

--- page.php
+++ page.php
@@ -1,8 +1,18 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying pages
+ *
+ * @package simplyBasic
+ * @since simplyBasic 0.0.0
+ */
 
-<?php while( have_posts() ) {
-  the_post();
-  get_template_part('template-parts/content/content');
-} ?>
+get_header(); ?>
+
+<?php if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+    get_template_part_by_slug();
+  }
+}?>
 
 <?php get_footer();
