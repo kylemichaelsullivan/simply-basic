0ee80b3bfdb9de62ec3db9472ff5f861dd5afdfa
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-09 04:19:15 -0400

    Add meta data to index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
+<?php
+/**
+ * This is the template for index.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package simplyBasic
+ * @since 0.0.0
+ */
+?>
+
 <?php // Read XKCD
   wp_footer();
