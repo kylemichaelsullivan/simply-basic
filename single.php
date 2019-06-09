d6a8bae909bd9b1e871a241c06a190d67b016e5d
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-09 04:23:42 -0400

    Update single.php

--- single.php
+++ single.php
@@ -1,5 +1,52 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package simplyBasic
+ * @since 0.0.0
+ */
 
-<h1><?= the_title(); ?></h1>
+get_header();
+?>
 
-<?php get_footer();
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php while( have_posts() ) {
+				the_post();
+
+				get_template_part( 'template-parts/content/content', 'single' );
+
+				if( is_singular( 'attachment' ) ) {
+					// Parent post navigation.
+					the_post_navigation(
+						array(
+							/* translators: %s: parent post link */
+							'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'simplyBasic' ), '%title' ),
+						)
+					);
+				} elseif( is_singular( 'post' ) ) {
+					// Previous/next post navigation.
+					the_post_navigation(
+						array(
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'simplyBasic' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Next post:', 'simplyBasic' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'simplyBasic' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Previous post:', 'simplyBasic' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+						)
+					);
+				}
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+			} ?>
+		</main>
+	</section>
+
+<?php
+get_footer();
